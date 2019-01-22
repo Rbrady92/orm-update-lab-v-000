7bfbc3007b477a9373611265e5b7c3c573f2c32a
Ryan Brady <ryanbrady369@gmail.com>
2019-01-22 22:16:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,42 @@
 require_relative "../config/environment.rb"
 
 class Student
+  attr_accessor :id, :name, :grade
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize(id = nil, name, grade)
+    @id = id 
+    @name = name 
+    @grade = grade
+  end 
 
+  def self.create_table
+    DB[:conn].execute("CREATE TABLE IF NOT EXISTS students
+                       id INTEGER PRIMARY KEY,
+                       name TEXT,
+                       grade TEXT
+                     ")
+  end 
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE students")
+  end 
+
+  def save 
+    DB[:conn].execute("INSERT INTO students (name, grade) VALUES (?, ?)", self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end 
+
+  def self.create(name, grade)
+    student = self.new(name, grade)
+    student.save
+  end 
+
+  def self.new_from_db(info_arr)
+    self.new(row[0], row[1], row[2])
+  end 
+
+  def find_by_name(name)
+    DB[:conn].execute("SELECT * FROM students WHERE name = ?", name).map {|row| self.new_from_db(row)}.first
+  end     
 
 end
