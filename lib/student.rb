83b1f67dfe6f8946b1346c6a2d3b421211daaca0
Ryan Brady <ryanbrady369@gmail.com>
2019-01-22 22:16:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,10 +4,10 @@ class Student
   attr_accessor :id, :name, :grade
 
   def initialize(id = nil, name, grade)
-    @id = id 
-    @name = name 
+    @id = id
+    @name = name
     @grade = grade
-  end 
+  end
 
   def self.create_table
     DB[:conn].execute("CREATE TABLE IF NOT EXISTS students
@@ -15,28 +15,28 @@ class Student
                        name TEXT,
                        grade TEXT
                      ")
-  end 
+  end
 
   def self.drop_table
     DB[:conn].execute("DROP TABLE students")
-  end 
+  end
 
-  def save 
+  def save
     DB[:conn].execute("INSERT INTO students (name, grade) VALUES (?, ?)", self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-  end 
+  end
 
   def self.create(name, grade)
     student = self.new(name, grade)
     student.save
-  end 
+  end
 
   def self.new_from_db(info_arr)
     self.new(row[0], row[1], row[2])
-  end 
+  end
 
   def find_by_name(name)
     DB[:conn].execute("SELECT * FROM students WHERE name = ?", name).map {|row| self.new_from_db(row)}.first
-  end     
+  end
 
 end
